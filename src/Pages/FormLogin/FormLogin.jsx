5dbc74df3c0a778445d5083f67f45d114300283f
ARASHGHANABRZADEH <arashghanbarzadeh0902@gmail.com>
2025-04-27 09:48:53 +0330

    save username and password ind jasonfile

--- src/Pages/FormLogin/FormLogin.jsx
+++ src/Pages/FormLogin/FormLogin.jsx
@@ -1,12 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
 const FormLogin = () => {
   const form = useForm();
   const { register } = form;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate("");
+  const pageHome = () => {
+    navigate("/profile");
+  };
+  const handellSubmit = async e => {
+    e.preventDefault();
+    console.log(userName, password);
+    await axios.post("http://localhost:3001/loginUser", {
+      userName: userName,
+      password: password,
+    });
+    setUserName("");
+    setPassword("");
+  };
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#959D95]">
       <form
+        onSubmit={handellSubmit}
         action="#"
+        method="POST"
         className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
         <h2 className="text-2xl font-bold text-center text-amber-800 mb-6">
           Login
@@ -22,7 +43,8 @@ const FormLogin = () => {
             type="text"
             {...register("userName")}
             id="userName"
-            placeholder="Enter your username"
+            placeholder="Ent er your username"
+            onChange={e => setUserName(e.target.value)}
             className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:ring-2 focus:ring-amber-800 placeholder-gray-400 text-gray-800"
           />
         </div>
@@ -35,15 +57,17 @@ const FormLogin = () => {
           </label>
           <input
             type="password"
-            {...register("passWord")}
+            {...register("password")}
             id="passWord"
             placeholder="Enter your password"
+            onChange={e => setPassword(e.target.value)}
             className="w-full border border-gray-300 rounded-md p-3 focus:outline-none focus:ring-2 focus:ring-amber-800 placeholder-gray-400 text-gray-800"
           />
         </div>
 
         <button
           type="submit"
+          onClick={pageHome}
           className="w-full bg-amber-800 hover:bg-amber-700 text-white font-bold py-3 rounded-md transition duration-300">
           Login
         </button>
